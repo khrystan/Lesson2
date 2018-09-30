94d56bc45611a2e3c683d863ee05be7cf3e45658
Home <Home@DESKTOP-QA429QB>
2018-09-30 16:51:15 +0300

    added

---
+++ Lesson2/src/com/gmail/ozyura1985/Main.java
@@ -0,0 +1,28 @@
+package com.gmail.ozyura1985;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a,b,c,d,e,g;
+		a=9;
+		b=3;
+		c = a+b;
+		d = a-b;
+		e = a/b;
+		g = a*b;
+		System.out.println("c="+c);	
+		System.out.println("d="+d);
+		System.out.println("e="+e);
+		System.out.println("g="+g);
+		
+		double da=9.0,db=3.0;
+		double dc=da+db;
+		double dd=da-db;
+		double de=da*db;
+		double dg=da/db;
+		System.out . println ( "dc = "+ dc);
+		System.out . println ( "dd = "+ dd);
+		System.out . println ( "de = "+ de);
+		System.out . println ( "dg = "+ dg);
+	}
+}
